c4cbbf306f19e50587c4cab6bec9b5546d36f84b
rimshidali <rimshidali105@gmail.com>
2025-07-22 18:46:12 +0000

    added functional code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MlPipeline",
+    version= "0.0.0",
+    author="rimsjid",
+    author_email="rrimshid@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
